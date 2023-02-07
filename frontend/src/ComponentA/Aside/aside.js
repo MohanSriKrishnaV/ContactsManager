f94152cea561821d3c2804a8e668e144d8169641
Akash kumar Singh <akashkumarsingh2727@gmail.com>
2023-02-07 17:28:16 +0530

    first

---
+++ frontend/src/ComponentA/Aside/aside.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./aside.css";
+
+const Aside=()=>{
+    return(
+        <>
+        
+        </>
+    )
+}
+export default Aside;
---
+++ frontend/src/ComponentA/Contacts/contacts.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./contacts.css";
+
+const Contacts=()=>{
+    return(
+        <>
+        
+        </>
+    )
+}
+export default Contacts;
