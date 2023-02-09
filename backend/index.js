43267ef2872f323c3deb9ffd48b813a6ad4bdee1
Meet Shah <shahmeet97592@gmail.com>
2023-02-09 10:42:24 +0530

    created api for registration and for login with token

--- backend/index.js
+++ backend/index.js
@@ -3,9 +3,14 @@ const mongoose = require("mongoose");
 const app = express();
 const user = require('./models/User');
 const contact = require('./models/Contact');
+const registrationRoute = require('./routes/registration')
+const loginRoute = require('./routes/login');
 const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
+const cors = require("cors");
 const port = 8086;
 app.use(express.json());
+app.use(cors());
 
 const url = `mongodb+srv://admin:admin@cluster0.kzqiv0i.mongodb.net/?retryWrites=true&w=majority`
 
@@ -16,5 +21,8 @@ mongoose.connect(url, (err)=>{
         console.log("connection successful")
     }
 })
+
+app.use('/api', registrationRoute);
+app.use('/api', loginRoute);
 
 app.listen(port, () => console.log(`App listening to port ${port}`))
